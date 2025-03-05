6fd107f3e59fcf422adaa3b965786a2d2f83512d
tanjabjorklind <tbjorklind@gmail.com>
2025-03-05 10:36:27 +0100

    testing

--- app/components/test.tsx
+++ app/components/test.tsx
@@ -2,9 +2,18 @@
 type MyComponentProps = {
   name: string;
 };
-
+//testing testing testing
 const MyComponent = ({ name }: MyComponentProps) => {
   return (
+    <div className="ml-10 bg-blue-500 text-white p-4 rounded-md shadow-lg">
+      <h1 className="text-2xl font-bold">Hello, {name}!</h1>
+    </div>
+    <div className="ml-10 bg-blue-500 text-white p-4 rounded-md shadow-lg">
+      <h1 className="text-2xl font-bold">Hello, {name}!</h1>
+    </div>
+    <div className="ml-10 bg-blue-500 text-white p-4 rounded-md shadow-lg">
+      <h1 className="text-2xl font-bold">Hello, {name}!</h1>
+    </div>
     <div className="ml-10 bg-blue-500 text-white p-4 rounded-md shadow-lg">
       <h1 className="text-2xl font-bold">Hello, {name}!</h1>
     </div>
